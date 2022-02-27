77fd97ae34de89ccfbfb7781f2bb74f0a7decee1
toti90 <tothtomi7@gmail.com>
2022-02-27 14:43:13 +0100

    add base of loginPage

--- money_planner/src/pages/LoginPage.tsx
+++ money_planner/src/pages/LoginPage.tsx
@@ -1,7 +1,37 @@
+import { Card, CardHeader, TextField } from '@mui/material';
 import React from 'react';
 
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Wrapper>
+      <Card sx={{ display: 'flex', flexDirection: 'column', padding: '50px' }}>
+        <CardHeader title="Login" subheader="Please login to the site" />
+        <TextField
+          required
+          id="outlined-required"
+          label="E-mail"
+          type="email"
+          sx={{ marginBottom: '20px' }}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Password"
+          type="password"
+        />
+      </Card>
+    </Wrapper>
+  );
 };
 
 export default LoginPage;
